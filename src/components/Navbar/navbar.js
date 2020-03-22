098353a04a0dce3273ac5df9b03306af884087b3
Guilherme Tavares <guilhermetavalves@gmail.com>
2020-03-21 22:31:03 -0300

    adiciona styled components e componente de header na aplicacao

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Header, Title } from './styled'
+
+const Navbar = ({ }) => (
+  <Header>
+   <Title>Covid19 status</Title>
+  </Header>
+  )
+  
+  
+  export default Navbar
+  
---
+++ src/components/Navbar/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Header = styled.header`
+    padding: 20px 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #FF5D73;
+`
+const Title = styled.h1`
+    color: #ffffff;
+    font-family: 'Nunito', sans-serif;
+`
+
+export { Header, Title }
